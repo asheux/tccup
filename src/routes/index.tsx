fc986c069e472eefe579a89cd59ded6383eae065
asheux <brian.mboya@protonmail.com>
2024-10-26 21:46:47 +0300

    Fix create thought

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,29 @@
 import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import FallBackError from "src/components/FallBackError";
 const PublicPage = lazy(() => import("src/containers/PublicPage"));
 // const ProjectsArchive = lazy(() => import("src/components/ProjectsArchive"));
 // const Archive = lazy(() => import("src/components/Archive"));
-import { PreviousRouteProvider } from "./context";
 import { Spinner } from "src/commons/Loader";
 import "src/main.css";
 
-const AppRoutes = (props) => {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PublicPage />,
+    children: [
+      {
+        path: "*",
+        element: <PublicPage />,
+      },
+    ],
+  },
+]);
+
+const AppRoutes = () => {
   return (
     <Suspense fallback={<Spinner />}>
-      <BrowserRouter future={{ v7_startTransition: true }}>
-        <PreviousRouteProvider>
-          <Routes>
-            <Route>
-              <Route path="/" element={<PublicPage {...props} />}></Route>
-              <Route element={<FallBackError />}></Route>
-            </Route>
-          </Routes>
-        </PreviousRouteProvider>
-      </BrowserRouter>
+      <RouterProvider router={router} fallbackElement={<Spinner />} />
     </Suspense>
   );
 };
